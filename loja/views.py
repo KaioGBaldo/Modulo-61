cc11b3b39fe2bde02c23914441450435d1c7c92a
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-02-23 09:32:09 -0300

    Update views.py

--- loja/views.py
+++ loja/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
-# View para a lista de posts
 def lista_posts(request):
-    posts = Post.objects.filter(status='publicado') # Pega só os publicados
+    # Requisito: Dados enviados pela view
+    posts = Post.objects.filter(status='publicado') 
     return render(request, 'loja/lista_posts.html', {'posts': posts})
 
-# View para o detalhe do post
 def detalhe_post(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'loja/detalhe_post.html', {'post': post})
