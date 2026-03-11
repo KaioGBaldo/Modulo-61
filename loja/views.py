598146ea0e8431c526f65375919c930b490a28ac
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-10 22:21:34 -0300

    Update views.py

--- loja/views.py
+++ loja/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post  # Agora o Post existirá
+from .models import Post
 
 def lista_posts(request):
-    # Filtra apenas os posts com status 'publicado'
+    # Busca apenas posts publicados para exibir na lista
     posts = Post.objects.filter(status='publicado') 
     return render(request, 'loja/lista_posts.html', {'posts': posts})
 
 def detalhe_post(request, slug):
+    # Busca o post pelo slug ou retorna 404 se não existir
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'loja/detalhe_post.html', {'post': post})
