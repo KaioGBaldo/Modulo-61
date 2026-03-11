3994390e585eebc07451dea5c4fdc8387158af6c
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-10 22:21:17 -0300

    Update urls.py

--- loja/urls.py
+++ loja/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('loja.urls')),
+    path('blog/', views.lista_posts, name='lista_posts'),
+    path('blog/<slug:slug>/', views.detalhe_post, name='detalhe_post'),
 ]
