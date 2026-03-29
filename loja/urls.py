6a4319116476eceff7009eedabf1d7a3255c6786
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:13:58 -0300

    Update urls.py

--- loja/urls.py
+++ loja/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'loja'
 
 urlpatterns = [
-    # Deixe vazio por enquanto ou adicione rotas futuras aqui
+    path('', views.lista_posts, name='lista_posts'),
+    path('post/<int:pk>/', views.detalhe_post, name='detalhe_post'),
 ]
