84d936819ff26c0350d9c1cede850fce0ed7c3fe
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:13:37 -0300

    Update models.py

--- loja/models.py
+++ loja/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Product(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    active = models.BooleanField(default=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
