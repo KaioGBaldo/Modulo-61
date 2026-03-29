553163ee6b09fd90beed9dcd8a80579a563e39ce
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:10:56 -0300

    Refactor admin.py to use Product model

--- loja/admin.py
+++ loja/admin.py
@@ -1,20 +1,6 @@
 from django.contrib import admin
-from .models import Produto, Estoque, Post
+from .models import Product
 
-@admin.register(Produto)
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'preco', 'criado_em')
-    search_fields = ('nome',)
-    list_filter = ('criado_em',)
-
-@admin.register(Estoque)
-class EstoqueAdmin(admin.ModelAdmin):
-    list_display = ('id', 'produto', 'quantidade', 'local')
-    search_fields = ('produto__nome',)
-    list_filter = ('local',)
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'status', 'criado_em')
-    prepopulated_fields = {'slug': ('titulo',)} # Facilita a criação do slug
-    search_fields = ('titulo',)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'active')
