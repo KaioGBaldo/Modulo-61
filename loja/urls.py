aa2fb7b63e87ca23d486a711d269a21b7fcb1adf
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:08:57 -0300

    Remove blog URL patterns from urls.py
    Removed blog URL patterns from urlpatterns.

--- loja/urls.py
+++ loja/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.lista_posts, name='lista_posts'),
-    path('blog/<slug:slug>/', views.detalhe_post, name='detalhe_post'),
+    # path('', views.index, name='index'),
 ]
