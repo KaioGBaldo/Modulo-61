d5b3dff75102fe8df91c4939a16959345c19f085
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:11:11 -0300

    Reformat urls.py for consistency and clarity

--- setup_django/urls.py
+++ setup_django/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('loja.urls')),
+    path('', include('loja.urls')), # Conecta com loja/urls.py
 ]
