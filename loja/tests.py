69f14620ca047df4fb78123661d3ea79e36bdb57
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:14:09 -0300

    Add post creation tests with factories

--- loja/tests.py
+++ loja/tests.py
@@ -1,21 +1,22 @@
 import pytest
 import factory
-from loja.models import Product
+from django.contrib.auth.models import User
+from loja.models import Post
 
-# Cria uma fábrica de produtos para os testes
-class ProductFactory(factory.django.DjangoModelFactory):
+class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = Product
+        model = User
+    username = factory.Sequence(lambda n: f'user_{n}')
 
-    title = 'Produto Padrão'
-    description = 'Descrição do produto de teste'
-    price = 50.00
-    active = True
+class PostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Post
+    title = 'Meu Post de Exemplo'
+    content = 'Conteúdo do post para o teste EBAC.'
+    author = factory.SubFactory(UserFactory)
 
 @pytest.mark.django_db
-def test_create_product():
-    # Cria um produto real no banco de dados de teste
-    product = ProductFactory(title="Teclado Mecânico")
-    
-    assert product.title == "Teclado Mecânico"
-    assert Product.objects.count() == 1
+def test_create_post():
+    post = PostFactory()
+    assert Post.objects.count() == 1
+    assert post.title == 'Meu Post de Exemplo'
