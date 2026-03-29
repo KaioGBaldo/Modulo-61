87f3f6fd2de37c60370e7c5cce1a2999afebbfc5
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:13:46 -0300

    Refactor post views to use published status and primary key

--- loja/views.py
+++ loja/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def lista_posts(request):
-    # Busca apenas posts publicados para exibir na lista
-    posts = Post.objects.filter(status='publicado') 
-    return render(request, 'loja/lista_posts.html', {'posts': posts})
+    posts = Post.objects.filter(published=True)
+    return render(request, 'lista_posts.html', {'posts': posts})
 
-def detalhe_post(request, slug):
-    # Busca o post pelo slug ou retorna 404 se não existir
-    post = get_object_or_404(Post, slug=slug)
-    return render(request, 'loja/detalhe_post.html', {'post': post})
+def detalhe_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'detalhe_post.html', {'post': post})
