28619265f7b002f87fe88f2a36e14cd49a5546e8
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-04-09 21:51:42 -0300

    Delete loja directory

--- loja/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'loja'
-
-urlpatterns = [
-    path('', views.lista_posts, name='lista_posts'),
-    path('post/<int:pk>/', views.detalhe_post, name='detalhe_post'),
-]
